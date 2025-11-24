01e4ce6ce70db4afa692c7c7c6908d6eb6d5e119
eya <eyabredai123@gmail.com>
2025-11-24 15:05:07 +0100

    Résolution des conflits

--- src/main/java/com/fst/AppVenteEnLigne/entities/Order.java
+++ src/main/java/com/fst/AppVenteEnLigne/entities/Order.java
@@ -8,9 +8,12 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
 
 @Entity
+@Table(name = "`order`")
+
 public class Order {
 	@JoinColumn(name = "user_id") 
 	@ManyToOne
